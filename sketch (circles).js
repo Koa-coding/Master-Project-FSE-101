b8cd40d43f8e4bf9e69345f14b6544362b2710f5
koa-2101 <69717348+Koa-coding@users.noreply.github.com>
2022-10-12 04:32:18 +0700

    Update sketch (circles).js
    made it so that every time you run the code a circle with a different colour and position appears

--- sketch (circles).js
+++ sketch (circles).js
@@ -1,12 +1,20 @@
+let x, y;
+let r, g, b;
+let radius = 50;
+
 function setup() {
   createCanvas(710, 400);
-  fill(204, 101, 192, 127);
-  stroke(127, 63, 120);
+  x = random(710);
+  y = random(400);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
   background(0);
-  ellipse(240, 240, 80, 80);
+  fill(r, g, b)
+  ellipse(x, y, radius*3, radius*3);
 }
 
 function circleappear() {
