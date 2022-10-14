cbdda940bd3760804614474ca3387a464f4b48d3
mrboogiewoogie <106433960+mrboogiewoogie@users.noreply.github.com>
2022-10-14 13:33:48 -0700

    Update sketch (circles).js

--- sketch (circles).js
+++ sketch (circles).js
@@ -1,22 +1,52 @@
-let x, y;
-let r, g, b;
-let radius = 50;
+
+
 
 function setup() {
-  createCanvas(710, 400);
-  x = random(710);
-  y = random(400);
-  r = random(255);
-  g = random(255);
-  b = random(255);
-}
-
-function draw() {
-  background(0);
-  fill(r, g, b)
-  ellipse(x, y, radius*3, radius*3);
-}
-
-function circleappear() {
+  createCanvas(600, 600);
+  background(255);
+var circ = [];
   
+  var counter = 0;
+
+  
+  while (circ.length < 500) {
+		var circle = {
+			x: random(width),
+			y: random(height),
+			r: 20,
+		}
+
+    
+    var overlapping = false;
+    for (var j = 0; j < circ.length; j++) {
+      var other = circ[j];
+      var d = dist(circle.x, circle.y, other.x, other.y);
+      if (d < circle.r + other.r) {
+        overlapping = true;
+      }
+    }
+
+    
+    if (!overlapping) {
+      circ.push(circle);
+    }
+
+    
+    counter++;
+    if (counter > 6) {
+      break;
+    }
+  }
+
+  
+  for (var i = 0; i < circ.length; i++) {
+    // fill(255, 0, 175, 100);
+    let  s="1";
+    fill (random(255));
+    fill(s, random(255), random(255), 100);
+    noStroke();
+    ellipse(circ[i].x, circ[i].y, circ[i].r * 2, circ[i].r * 2);
+    
+    
+  }
 }
