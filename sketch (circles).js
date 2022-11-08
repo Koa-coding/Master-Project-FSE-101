21f8548348ab57d1a9baa3cbe87d599e339967aa
koa-2101 <69717348+Koa-coding@users.noreply.github.com>
2022-11-08 13:06:21 +0700

    Update sketch (circles).js

--- sketch (circles).js
+++ sketch (circles).js
@@ -1,10 +1,10 @@
 let bubbles = [];
 let score = 0;
-let level = 1;
+let level = 0;
 
 function setup() {
   createCanvas(600, 400);
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 15; i++) {
     let x = random(width);
     let y = random(height);
     let r = 40;
@@ -22,11 +22,31 @@ function mousePressed() {
 function draw() {
   background(0);
   noStroke()
+  fill('orange');
   text("Score: " + score, 10, 40);
-  textSize(30);
-  for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].show();
-    
+  text("Level: " +level, 10, 70)
+  textSize(25);
+    if (level == 0) {
+      splash();
+    }
+  
+    if((mouseIsPressed == true && level != 2) && (mouseIsPressed == true && level != 3)){
+	  level = 1;
+	}
+  
+    if (level == 1){
+      level1();  
+    }
+  
+    if (level == 2){
+      level2();
+    }
+  
+    if (level == 3){
+      level3();
+    }
+  
+}
     //var overlapping = false;
     //for (let i = 0; i < bubbles.length; i++) {
     //  var other = bubbles[i];
@@ -37,8 +57,43 @@ function draw() {
     //}
     //if (!overlapping){
     //}
+
+function splash(){
+	//appearance of game
+	background(150, 230, 240); //sky blue
+	
+	//title
+    fill(255);
+    stroke(0);
+    strokeWeight(3);
+    textSize(20);
+    //instructions
+    text('HOW TO PLAY:', 250, 100);
+    text('Click circles to get score', 200, 150);
+    text('CLICK THE SCREEN TO START', 170, 200);
+	
+}
+
+function level1() {
+  for (let i = 0; i < 5; i++) {
+    bubbles[i].appear(); 
   }
 }
+
+function level2() {
+  level = 2;
+  for (let i = 0; i < 5; i++) {
+    bubbles[i].appear(); 
+  }
+}
+
+function level3() {
+  level = 3;
+  for (let i = 0; i < 5; i++) {
+    bubbles[i].appear(); 
+  }
+}
+
 
 class Bubble {
   constructor(x, y, r) {
@@ -51,14 +106,22 @@ class Bubble {
   clicked(px, py) {
     let d = dist(px, py, this.x, this.y);
     if (d < this.r) {
-      this.col = color(0,0,0,0);
       score++;
+      this.col = color(0,0,0,0);
       strokeWeight(4)
       stroke(0)
+      if ((level == 1) && (score == 5)) {
+        level++;
+        score = 0;
+      }
+      if ((level == 2) && (score == 10)) {
+        level++;
+        score = 0;
+      }
     }
   }
 
-  show() {
+  appear() {
     stroke(255);
     strokeWeight(4)
     fill(this.col, 60);
